00869487ac970ebe3bcf59c835f1c84f670134f2
LouisPerry4281 <louisperry4281@gmail.com>
2023-02-02 10:22:44 +0000

    Rope Prototype

---
+++ Big Tree/Assets/RootRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootRenderer : MonoBehaviour
+{
+    [SerializeField] Transform[] points;
+    LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.positionCount = points.Length;
+    }
+
+    private void Update()
+    {
+        print(points[1].position);
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            //print(points[i].position);
+            lineRenderer.SetPosition(i, points[i].position);
+        }
+    }
+}
