c756a3457abd86ac46ba51a04a59cb5263355c3b
LouisPerry4281 <louisperry4281@gmail.com>
2023-02-02 09:15:57 +0000

    Amogus + Camera Facing

---
+++ Big Tree/Assets/CameraFacing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacing : MonoBehaviour
+{
+    Transform cameraPos;
+
+    private void Start()
+    {
+        cameraPos = GameObject.Find("Main Camera").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.LookAt(new Vector3(cameraPos.position.x, transform.position.y, cameraPos.position.z));
+    }
+}
