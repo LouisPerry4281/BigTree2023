fb16a89696c413e1e7c74ed4b924a1df1df2c940
Tom Prince <ttpzgaming@gmail.com>
2023-02-02 16:39:43 +0000

    Merge branch 'main' of https://github.com/LouisPerry4281/BigTree2023

---
+++ Big Tree/Assets/Scripts/Switch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField] private GameObject switchTarget;
+
+    public bool isFlipped;
+
+    private void Update()
+    {
+        if (isFlipped)
+        {
+            switchTarget.GetComponent<Animator>().SetTrigger("goDown");
+        }
+    }
+
+    public void FlipSwitch()
+    {
+        //if (isFlipped)
+            //return;
+
+        //switchTarget.GetComponent<BlockMover>().MoveMe();
+    }
+}
