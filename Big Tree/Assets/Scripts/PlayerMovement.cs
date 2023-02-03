c9b394784ee90ed4104934a6cb49f4a95aca51d0
LouisPerry4281 <louisperry4281@gmail.com>
2023-02-03 09:09:15 +0000

    BEES

--- Big Tree/Assets/Scripts/PlayerMovement.cs
+++ Big Tree/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    CharacterController controller;
+    public CharacterController controller;
 
-    [SerializeField] float speed = 12f;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jump = 1f;
 
-    private void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        Vector3 move = (transform.right * x) + (transform.forward * z);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
-        controller.SimpleMove(move.normalized * speed);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
